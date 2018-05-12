845a0cb80bef543ce8a92b10bea5c2e05a88455a
alimuhnad72 <alimuhnad72@gmail.com>
2018-05-12 10:50:42 +0300

    d

--- models/addhavecar.js
+++ models/addhavecar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 //يعرف اليوزر و الايميل 
 
@@ -15,21 +14,3 @@ const userSchema= new Schema({
 
 
 module.exports = mongoose.model('havecars',userSchema)
-=======
-var mongoose = require('mongoose');
-//يعرف اليوزر و الايميل 
-
-const Schema = mongoose.Schema
-const userSchema= new Schema({
-   drivername:String,
-   driverphonenum:{ type: String,unique: true },
-   wight:String,
-   cartype:String,
-   location:String,
-   createdtime:String
-
-})
-
-
-module.exports = mongoose.model('havecars',userSchema)
->>>>>>> 579ef09a10a1399df2da4ef70e64ebd980a25ae0
--- models/users.js
+++ models/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 //يعرف اليوزر و الايميل 
 
@@ -10,17 +9,3 @@ const userSchema= new Schema({
 
 
 module.exports = mongoose.model('users',userSchema)
-=======
-var mongoose = require('mongoose');
-//يعرف اليوزر و الايميل 
-
-const Schema = mongoose.Schema
-const userSchema= new Schema({
-    email:String,
-    password:String,
-    token:String
-})
-
-
-module.exports = mongoose.model('user',userSchema,'users')
->>>>>>> 579ef09a10a1399df2da4ef70e64ebd980a25ae0
