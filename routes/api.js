3046acc537e6100b92c79b7e4b3fbc2244898149
alimuhnad72 <alimuhnad72@gmail.com>
2018-05-14 09:49:01 +0300

    a

--- routes/api.js
+++ routes/api.js
@@ -164,7 +164,7 @@ router.post('/sendreport', function(req, res) {
     Reports.create({
 
         id: filter.clean(req.body.id),
-        title:req.body.data12 ,
+        title:{title:req.body.data12} ,
         phone:req.body.phone ,
         
     }, function(err, review) {
